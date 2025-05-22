c0b5a3be2de56932b8a12cc06769f3cc186e261b
marvkos <87067609+marvkos@users.noreply.github.com>
2025-05-22 10:53:02 +0200

    add main comment

--- src/main/java/at/technikum/Main.java
+++ src/main/java/at/technikum/Main.java
@@ -2,6 +2,7 @@ package at.technikum;
 
 public class Main {
     public static void main(String[] args) {
+        // new calculator instance
         Calculator calculator = new Calculator();
     }
 }
