1ea769dce9dddce26a5c1912b635def5dd75d26b
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2025-06-10 09:34:24 +0200

    remove comment

--- src/main/java/at/technikum/Main.java
+++ src/main/java/at/technikum/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         // new calculator instance
         Calculator calculator = new Calculator();
-        //calculator.sum(1, 2);
     }
 }
