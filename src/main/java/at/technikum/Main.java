2c672d6efdfffc0896c444d135f2c9a0f809db2e
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2025-06-10 09:29:14 +0200

    add checkstyle to pom.xml and rules

--- src/main/java/at/technikum/Main.java
+++ src/main/java/at/technikum/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         // new calculator instance
         Calculator calculator = new Calculator();
+        //calculator.sum(1, 2);
     }
 }
